066b3ea5124549e0ad0dde1ef95bc494a84c81b5
Markus Flambard <markus.flambard@youcal.com>
2022-10-31 22:41:16 +0100

    Add a few decode tests

--- sql/decode_test.sql
+++ sql/decode_test.sql
@@ -1,2 +1,3 @@
-select erlang_term_decode('\x836a')
--- select erlang_term_decode('\x8383770665726c616e67');
+select erlang_term_decode('\x836a');
+select erlang_term_decode('\x83770665726c616e67');
+select erlang_term_decode('\x836D0000000642494E415259');
